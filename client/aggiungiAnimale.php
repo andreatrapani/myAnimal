f7b76848742ad2f3bb0e76cd9b5dd01e0a7c9892
uf101 <106037172+uf101@users.noreply.github.com>
2023-05-19 23:35:07 +0200

    Update aggiungiAnimale.php

--- client/aggiungiAnimale.php
+++ client/aggiungiAnimale.php
@@ -6,9 +6,10 @@ $sesso = filter_var(trim($_POST['sesso']),FILTER_SANITIZE_STRING);
 $peso = filter_var(trim($_POST['peso']),FILTER_SANITIZE_STRING);
 $data_nascita = filter_var(trim($_POST['data_nascita']),FILTER_SANITIZE_STRING);
 $colore = filter_var(trim($_POST['colore']),FILTER_SANITIZE_STRING);
+$ufk = filter_var(trim($_POST['ufk']),FILTER_SANITIZE_STRING);
 
 $connection= new PDO('mysql:host=localhost;dbname=reg-bd','root','');
-$sql='INSERT INTO animali (nome, tipologia, razza, sesso, peso, data_nascita, colore) VALUES (:nome, :tipologia, :razza, :sesso, :peso, :data_nascita, :colore)';
+$sql='INSERT INTO animali (nome, tipologia, razza, sesso, peso, data_nascita, colore, ufk) VALUES (:nome, :tipologia, :razza, :sesso, :peso, :data_nascita, :colore, :ufk)';
 
 $statement=$connection->prepare($sql);
 $statement->bindParam(':nome', $nome);
@@ -18,6 +19,7 @@ $statement->bindParam(':sesso', $sesso);
 $statement->bindParam(':peso', $peso);
 $statement->bindParam(':data_nascita', $data_nascita);
 $statement->bindParam(':colore', $colore);
+$statement->bindParam(':ufk', $ufk);
 
 $result=$statement->execute();
 if(!$result) {
@@ -26,6 +28,7 @@ if(!$result) {
     exit();
 }
 
-header('Location: /gestisciAnimale.html');
+//header('Location: /gestisciAnimale.html');
+
 exit();
 ?>
