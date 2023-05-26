9a495c5e9273bbc7eca608a09e66c8f1cb57e637
uf101 <106037172+uf101@users.noreply.github.com>
2023-05-26 11:21:09 +0200

    Update myVetdb.php

--- client/myVet/myVetdb.php
+++ client/myVet/myVetdb.php
@@ -19,6 +19,7 @@ if ($mysqli->connect_error) {
     die('Connection failed: ' . $mysqli->connect_error);
 }
 
+
 $sql = "SELECT * FROM user WHERE `uid`='$uid'";
 
 $result = $mysqli->query($sql);
@@ -28,14 +29,18 @@ if (!$result) {
     die('Error: ' . $mysqli->error);
 }
 
-// create an array to store the vet data
-$vets = array();
-
 // iterate over the rows and store the data in the array
 while ($row = $result->fetch_assoc()) {
     $vfk = $row['vfk'];
+    $nome = $row['nome'];
+    $cognome = $row['cognome'];
+    setcookie('nome', $nome, time()-3600, "/");
+    setcookie('nome', $nome, time()+3600, "/");
+    setcookie('cognome', $cognome, time()-3600, "/");
+    setcookie('cognome', $cognome, time()+3600, "/");
 }
 
+//setcookie('nome', $user['nome'], time()+3600, "/");
 
 // prepare a SQL query to retrieve data from the "vet" table based on UID and ufk comparison
 $sql = "SELECT * FROM vet WHERE `vid`='$vfk'";
@@ -68,10 +73,42 @@ while ($row = $result->fetch_assoc()) {
     array_push($vets, $vet);
 }
 
+
+
+
+$sql = "SELECT * FROM vet";
+
+// execute the query and retrieve the data
+$result = $mysqli->query($sql);
+
+// check for errors
+if (!$result) {
+    die('Error: ' . $mysqli->error);
+}
+
+// create an array to store the vet data
+$vetall = array();
+
+// iterate over the rows and store the data in the array
+while ($row = $result->fetch_assoc()) {
+    $vetl = array(
+        'vid' => $row['vid'],
+        'Nome' => $row['nome'],
+        'Cognome' => $row['cognome'],
+        'DataN' => $row['dataN'],
+        'Email' => $row['email'],
+        'Telefono' => $row['telefono'],
+        'Indirizzo' => $row['indirizzo'],
+        'Lat' => $row['lat'],
+        'Lon' => $row['lon'],
+        'ufk' => $row['ufk']
+    );
+    array_push($vetall, $vetl);
+}
 // close the database connection
 $mysqli->close();
 
 // pass the array to the calling script
-header("Location: myVet.php");
+//header("Location: myVet.php");
 
 ?>
